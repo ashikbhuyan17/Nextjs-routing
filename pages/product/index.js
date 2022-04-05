573085bc895d63c6227d9395af2ec285cf6e80d0
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-04-05 22:00:06 +0600

    dynamic routing

---
+++ pages/product/index.js
@@ -0,0 +1,12 @@
+
+const Product = () => {
+    return (
+        <div>
+            <h2>product list 1</h2>
+            <h2>product list 2</h2>
+            <h2>product list 3</h2>
+        </div>
+    );
+};
+
+export default Product;
