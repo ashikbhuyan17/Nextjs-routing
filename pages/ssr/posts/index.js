1338c39ccd258620b89ec6b78d79924156ba7970
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-08-15 16:53:24 +0600

    server side rendering

---
+++ pages/ssr/posts/index.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+const Post = ({ posts }) => {
+    return (
+        <>
+            <h1>list of post</h1>
+            {
+                posts.map((post) => {
+                    return (
+                        <div key={post.id}>
+                            <Link href={`posts/${post.id}`} passHref>
+                                <h2>{post.id} : {post.title}</h2>
+                            </Link>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
+        </>
+    );
+};
+
+export default Post;
+
+
+// export async function getStaticProps() {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+//     const data = await response.json()
+//     // console.log(data);
+//     return {
+//         props: {
+//             posts: data
+//         }
+//     }
+// }
+
+export async function getServerSideProps() {
+    console.log('Pre-rendering NewsArticleList')
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+
+    return {
+        props: {
+            posts: data
+        }
+    }
+}
